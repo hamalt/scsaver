75b5b7eca4ee36439f38e4937f3d919cb4da4596
hamalt <contact@hamalt.studio>
2022-08-14 00:49:51 +0900

    #6 Added Cypress setting

---
+++ cypress/support/commands.js
@@ -0,0 +1,66 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('scsaverPage', { prevSubject: 'optional' }, () => {
+  return cy.visit('cypress/test.html');
+});
+
+Cypress.Commands.add(
+  'initScsaver',
+  { prevSubject: 'optional' },
+  (subject, config = {}, el = '.scsaver') => {
+    return cy.window().then((_window) => {
+      _window.document.body.innerHTML = `
+      ${
+        config.progressBarParent
+          ? `
+      <div class="scsaver progress-bar-parent"></div>`
+          : ''
+      }
+      <div class="scsaver black-rainbow">
+        <div class="scsaver-inner">
+            <p>Hello, Scsaver.</p>
+        </div>
+      </div>
+      `;
+      // eslint-disable-next-line dot-notation
+      const _config = config;
+      if (!_config.waitTime) {
+        _config.waitTime = 2000;
+      }
+      if (config.events) {
+        _config.events = [
+          'mousemove',
+          'click'
+        ];
+      }
+      if (config.progressBar) {
+        _config.progressBar = true
+      }
+      _window.scsaverRef = new _window.Scsaver(el, _config);
+      return _window.scsaverRef;
+    });
+  },
+);
